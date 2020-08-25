b20597bb342553d4b2fe3cb16c9c35400cfb503c
qwalley <willalley345@gmail.com>
2020-08-25 15:25:56 -0400

    created embed for LFG posts, complete with voice channel invite

--- commands/createInvite.js
+++ commands/createInvite.js
@@ -1,12 +1,24 @@
 const config = require('../config.json');
+const Discord = require('discord.js');
 
-module.exports = (args) => {
+const embed = (description, author, invite, role) => {
+	return inviteEmbed = new Discord.MessageEmbed()
+		// .setColor('#0099ff')
+		.setTitle(description)
+		.setAuthor(author.nickname, author.user.avatarURL())
+		.setDescription(`Join them on Discord, in [${invite.channel.name}](${invite})`)
+		.addField('LFG Role:', `<@&${role}>`, false)
+		.setTimestamp()
+		.setFooter('React with ❌ to delete this message')
+}
+
+module.exports = async (args) => {
 	const msg = args.msg;
 	const guild = msg.client.guilds.resolve(config.master);
 	const voice = guild.voiceStates.resolve(msg.author.id);
 	
 	// delete message to keep channel clean
-	msg.delete();
+	await msg.delete();
 
 	if (!(voice && voice.channel)) {
 		// tell user to be in a voice channel on the main server
@@ -19,9 +31,11 @@ module.exports = (args) => {
 		msg.author.send('Please provide a description of what you\'re doing.');
 		return;
 	}
-	
 	const description = args.description.join(' ');
-	// plaintext message is a placeholder, should send a "pretty" embed
-	msg.channel.send(description + `\n<@&${config.tag_roles[args.role]}> <@${msg.author.id}>`)
+	const author = guild.members.resolve(msg.author.id);
+	const channel = guild.channels.resolve(voice.channel);
+	let invite = await channel.createInvite();
+	
+	msg.channel.send(embed(description, author, invite, config.tag_roles[args.role]))
 		.then(message => message.react('❌'))
 }
