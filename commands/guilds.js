aca9945b09fe073e488cceb58ab66b502285f8e9
qwalley <willalley345@gmail.com>
2020-09-03 14:10:42 -0400

    improved output formatting of the guilds command

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,8 +1,32 @@
+const channelString = (channel) => {
+	dashes = '--------------------------------------------------';
+	const name = channel.type === 'category' ? channel.name : '    ' + channel.name;
+	return `\t${(name + ' ' + dashes).slice(0, 50)} (${channel.id}, ${channel.type})`;
+}
+
 module.exports = {
 	name: 'guilds',
 	description: 'lists all available guilds',
 	execute(msg, args) {
-		let guilds = msg.client.guilds.cache.map(g => g.name).join('\n')
-		msg.reply(`The guilds available to this Bot are:\n${guilds}`)
+		let guilds = msg.client.guilds.cache.map((guild) => {
+			let channels = {}
+			let ret = `${guild.name}:`;
+			guild.channels.cache.map((channel) => {
+				if (channel.type === 'category') {
+					if (typeof channels[channel.id] === 'undefined') channels[channel.id] = [];
+					channels[channel.id].unshift(channelString(channel))
+				}
+				else {
+					if (typeof channels[channel.parentID] === 'undefined') channels[channel.parentID] = [];
+					channels[channel.parentID].push(channelString(channel))
+				}
+			});
+
+			for (const category in channels) {
+				ret += `\n${channels[category].join('\n')}`;
+			}
+			return ret;
+		});
+		msg.reply('```' + guilds.join('\n') + '```');
 	}
 }
